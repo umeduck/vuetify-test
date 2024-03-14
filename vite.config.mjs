4246f5c5b19e13d1ae3da757d2ee3d7bd1ebc4c8
umeduck <ume_duck@outlook.jp>
2024-03-14 22:09:52 +0900

    eslint

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,12 @@
 // Plugins
-import Components from 'unplugin-vue-components/vite'
-import Vue from '@vitejs/plugin-vue'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import Components from 'unplugin-vue-components/vite';
+import Vue from '@vitejs/plugin-vue';
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import ViteFonts from 'unplugin-fonts/vite';
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -44,4 +44,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-})
+});
